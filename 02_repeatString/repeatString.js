bcaae082e073a9f0d3dc92079ad225cb83757b6e
Kandy-Hamisi <hkandy2000@gmail.com>
2022-04-18 03:48:31 +0300

    finished repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(myString, num) {
 
+    num = 3;
+
+   if(num > 0) {
+       return myString.repeat(num);
+   }else {
+       return "";
+   }
+    
 };
+
+repeatString("hello");
 
 // Do not edit below this line
 module.exports = repeatString;
